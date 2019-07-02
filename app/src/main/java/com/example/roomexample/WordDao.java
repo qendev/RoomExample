98aae6372d160c4a95980c4058c0f6aecef07f06
qendev <qendev00@gmail.com>
2019-07-02 11:40:11 +0300

    First Commit

---
+++ app/src/main/java/com/example/roomexample/WordDao.java
@@ -0,0 +1,28 @@
+package com.example.roomexample;
+
+import java.util.List;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+//Daos have to be either interfaces or abstract classes because we dont provide method body.
+//We just create a method and annotate it and room does the rest.
+
+@Dao
+public interface WordDao {
+
+    @Insert
+    void insert(Word word);
+
+    @Query("DELETE FROM word_table")
+    void deleteAll();
+
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    LiveData<List<Word>>getAllWord();
+
+
+}
